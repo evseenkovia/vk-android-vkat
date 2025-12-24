34d83d0a87fde9625fe3bc41101b9031a105ccbd
Boogie-Woogie9 <evseenkov.2003@mail.ru>
2025-12-24 15:11:32 +0300

    feat: add TopAppBar with filter buttons

--- app/src/main/java/com/example/vk_android_vkat/ui/explore/ExploreFragment.kt
+++ app/src/main/java/com/example/vk_android_vkat/ui/explore/ExploreFragment.kt
@@ -5,6 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.vk_android_vkat.databinding.FragmentExploreBinding
@@ -13,8 +29,6 @@ class ExploreFragment : Fragment() {
 
     private var _binding: FragmentExploreBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -26,17 +40,64 @@ class ExploreFragment : Fragment() {
             ViewModelProvider(this).get(ExploreViewModel::class.java)
 
         _binding = FragmentExploreBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
         val textView: TextView = binding.textHome
         exploreViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
-        return root
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MainScreen(exploreViewModel)
+            }
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    @Composable
+    fun MainScreen(viewModel: ExploreViewModel) {
+        Scaffold(
+            topBar = { ToolBar() },
+        ){ innerPadding ->
+            ScrollContent(innerPadding)
+        }
+    }
+
+    @Composable
+    fun ToolBar() {
+        LazyRow(
+            modifier = Modifier.
+            padding(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            item {
+                AssistChip(
+                    onClick = { },
+                    label = { Text("Рядом") },
+                    leadingIcon = {
+                        Icon(Icons.Default.LocationOn, contentDescription = null)
+                    }
+                )
+            }
+            item { AssistChip(onClick = { }, label = { Text("Цель") }) }
+            item { AssistChip(onClick = { }, label = { Text("Местность") }) }
+            item { AssistChip(onClick = { }, label = { Text("Время суток") }) }
+            item { AssistChip(onClick = { }, label = { Text("Длительность") }) }
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun AppBarPreview(){
+        ToolBar()
+    }
+
+    @Composable
+    fun ScrollContent(innerPadding: PaddingValues) {
+    }
+
 }
