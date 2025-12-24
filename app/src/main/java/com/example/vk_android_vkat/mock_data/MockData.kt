5f84e0cbfb07417fbe0fe6945e73a2fd45d3bf21
Boogie-Woogie9 <evseenkov.2003@mail.ru>
2025-12-24 18:07:08 +0300

    feat: connect model to interface

---
+++ app/src/main/java/com/example/vk_android_vkat/mock_data/MockData.kt
@@ -0,0 +1,67 @@
+package com.example.vk_android_vkat.mock_data
+
+import com.example.vk_android_vkat.domain.model.RouteUi
+
+val mockRoutes = listOf(
+    RouteUi(
+        id = 1,
+        title = "Прогулка по старому городу",
+        description = "Исторический маршрут по центральной части города",
+        distanceKm = 10,
+        durationHours = 2,
+        pointsCount = 7,
+        rating = 4.7f,
+        imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEHZLEG4vE90aZYXy-YtldelQ7QHWYwwB-NA&s"
+    ),
+    RouteUi(
+        id = 2,
+        title = "Лесной маршрут",
+        description = "Приятная прогулка по лесной местности",
+        distanceKm = 15,
+        durationHours = 4,
+        pointsCount = 12,
+        rating = 4.9f,
+        imageUrl = "https://static.wikia.nocookie.net/darksouls/images/1/19/Поселок_Олачиль_1.jpg/revision/latest/smart/width/386/height/259?cb=20160130220340&path-prefix=ru"
+    ),
+    RouteUi(
+        id = 3,
+        title = "На берегу озера",
+        description = "Маршрут вдоль живописного озера",
+        distanceKm = 8,
+        durationHours = 1,
+        pointsCount = 5,
+        rating = 4.5f,
+        imageUrl = "https://velid.ru/img/dark-souls/ds-1/sad-tjomnyh-kornej_00.webp"
+    ),
+    RouteUi(
+        id = 4,
+        title = "Горный перевал",
+        description = "Сложный маршрут через горные тропы",
+        distanceKm = 20,
+        durationHours = 6,
+        pointsCount = 10,
+        rating = 4.8f,
+        imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDlSdqAl6N1Ggd3KcOO4jpxZwcU9Jqi9NCqw&s"
+    ),
+    RouteUi(
+        id = 5,
+        title = "Парк развлечений",
+        description = "Прогулка с посещением достопримечательностей и кафе",
+        distanceKm = 5,
+        durationHours = 2,
+        pointsCount = 8,
+        rating = 4.3f,
+        imageUrl = "https://i.ytimg.com/vi/rLoWP2U2ceg/maxresdefault.jpg"
+    ),
+    RouteUi(
+        id = 6,
+        title = "Вдоль реки",
+        description = "Маршрут с живописными видами на реку и мосты",
+        distanceKm = 12,
+        durationHours = 3,
+        pointsCount = 9,
+        rating = 4.6f,
+        imageUrl = "https://preview.redd.it/irithyll-of-the-boreal-valley-is-amazing-v0-h5pxqkbc5qjf1.jpg?width=640&crop=smart&auto=webp&s=47b38caa075c4cf4fa64a77df29c9572a8256553"
+    )
+)
+
--- app/src/main/java/com/example/vk_android_vkat/ui/explore/ExploreFragment.kt
+++ app/src/main/java/com/example/vk_android_vkat/ui/explore/ExploreFragment.kt
@@ -7,8 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.AssistChip
@@ -16,6 +19,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
@@ -24,6 +28,8 @@ import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.vk_android_vkat.databinding.FragmentExploreBinding
+import com.example.vk_android_vkat.domain.model.RouteUi
+import com.example.vk_android_vkat.mock_data.mockRoutes
 
 class ExploreFragment : Fragment() {
 
@@ -62,7 +68,10 @@ class ExploreFragment : Fragment() {
         Scaffold(
             topBar = { ToolBar() },
         ){ innerPadding ->
-            ScrollContent(innerPadding)
+            ScrollContent(
+                innerPadding = innerPadding,
+                routes = mockRoutes
+                )
         }
     }
 
@@ -90,14 +99,36 @@ class ExploreFragment : Fragment() {
         }
     }
 
-    @Preview(showBackground = true)
-    @Composable
-    fun AppBarPreview(){
-        ToolBar()
-    }
+//    @Preview(showBackground = true)
+//    @Composable
+//    fun AppBarPreview(){
+//        ToolBar()
+//    }
 
     @Composable
-    fun ScrollContent(innerPadding: PaddingValues) {
+    fun ScrollContent(
+        innerPadding: PaddingValues,
+        routes: List<RouteUi> = mockRoutes,
+        onLoadNext: () -> Unit = {},
+    ) {
+        val bottomNavHeight = 56.dp
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                    top = innerPadding.calculateTopPadding(),
+                    bottom = innerPadding.calculateBottomPadding() + bottomNavHeight)
+        ) {
+            items(routes, key = { it.id }){ route ->
+                RouteCard(route = route)
+            }
+        }
+//
+//        item {
+//            LaunchedEffect(Unit) {
+//                onLoadNext()
+//            }
+//        }
     }
 
 }
